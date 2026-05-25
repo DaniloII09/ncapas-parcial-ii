b11563b1d28daed92a5049c715b6dbda4fffe960
DaniloII09 <00377223@uca.edu.sv>
2026-05-25 17:04:10 -0600

    add: project creation

---
+++ ncapas-parcial-ii/src/main/java/com/example/ncapasparcialii/NcapasParcialIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncapasparcialii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcapasParcialIiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcapasParcialIiApplication.class, args);
+    }
+
+}
---
+++ ncapas-parcial-ii/src/test/java/com/example/ncapasparcialii/NcapasParcialIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncapasparcialii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcapasParcialIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
